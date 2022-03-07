7b4abc647e40016be779b66d28c25c3b695376c6
gastonip <gastoniparraguirre@outlook.com>
2022-03-07 18:40:38 -0300

    Ejercicio clase 3

---
+++ src/components/NavBar.js
@@ -0,0 +1,18 @@
+import React from 'react'
+
+const NavBar = () => {
+  return (
+    <>
+        <header>
+            <h1>NFT Store</h1>
+            <nav>
+                <a href="#">Inicio</a>
+                <a href="#">Productos</a>
+                <a href="#">Contacto</a>
+            </nav>
+        </header>
+    </>
+  )
+}
+
+export default NavBar
